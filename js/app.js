20b8968c35dfbccb506cb43589463175ea01836a
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-10-08 21:51:42 -0300

    Inicio da criação da tela de introdução a seviço
    Faltando a inserção do carrosel para navegar entre os serviços ofertados

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function agendarManutencao(){
+    let ano = document.getElementById('ano');
+    let marca = document.getElementById('marca');
+    let modelo = document.getElementById('modelo');
+
+    console.log(ano.value);
+}
