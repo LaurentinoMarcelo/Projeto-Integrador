e39577b5da646db47c381641ba5060fcfaad1658
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2021-10-19 19:26:54 -0300

    corrigindo quebra do rolamento, e o bloqueio do final de semana no calendar

--- js/app.js
+++ js/app.js
@@ -98,3 +98,15 @@ function selecaoModelo(){
             
     }
 }
+
+
+
+const picker = document.getElementById('date1');
+picker.addEventListener('input', function(e){
+  var day = new Date(this.value).getUTCDay();
+  if([6,0].includes(day)){
+    e.preventDefault();
+    this.value = '';
+    alert('Desculpa final de semana não abrimos, agende um dia durante a semana.');
+  }
+});
