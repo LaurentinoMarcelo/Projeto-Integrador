9f69f6a9ebc152e727a6515e7219d00657e22bda
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2022-06-02 21:19:17 -0300

    Consumindo Api CarData

--- src/api/CarData.js
+++ src/api/CarData.js
@@ -1,10 +1,34 @@
-import axios from "axios";
+function criaLinha(cars){
+	linha = document.createElement("tr");
 
-export function ApiCardData(){
-	const options = {
+	tdId = document.createElement("td");
+	tdYear = document.createElement("td");
+	tdMake = document.createElement("td");
+	tdModel = document.createElement("td");
+	tdType = document.createElement("td");
+
+	tdId.innerHTML = cars.id;
+	tdYear.innerHTML = cars.year;
+	tdMake.innerHTML = cars.make;
+	tdModel.innerHTML = cars.model;
+	tdType.innerHTML = cars.type;
+
+	linha.appendChild(tdId);
+	linha.appendChild(tdYear);
+	linha.appendChild(tdMake);
+	linha.appendChild(tdModel);
+	linha.appendChild(tdType);
+
+	return linha;
+}
+
+function ApiCardData(){
+	alert("Eu sou um alert!");
+	
+		const options = {
 		method: 'GET',
 		url: 'https://car-data.p.rapidapi.com/cars',
-		params: {limit: '10', page: '0', model: 'elantra'},
+		params: {limit: '50', page: '0'},
 		headers: {
 		  'X-RapidAPI-Host': 'car-data.p.rapidapi.com',
 		  'X-RapidAPI-Key': '302cb51109mshbb577f353623720p17620cjsn56eb6909aaae'
@@ -12,9 +36,16 @@ export function ApiCardData(){
 	  };
 	  
 	  axios.request(options).then(function (response) {
-		  console.log(response.data);
+		 let cars = response.data;
+		let tabela = document.getElementById("tabela")
+		 cars.forEach(element => {
+			 let linha = criaLinha(element);
+			 tabela.appendChild(linha);
+		 });
+		 
 	  }).catch(function (error) {
 		  console.error(error);
 	  });
+  
 }
 
