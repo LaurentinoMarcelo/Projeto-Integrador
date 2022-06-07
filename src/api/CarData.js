972bef0008d7cdc104394e7a973a12c755d59d73
Marcelo Laurentino <marcelolaurentino56@gmail.com>
2022-06-07 18:00:43 -0300

    Implementando padrão da barra de navegação e estilização da tabela da Api

--- src/api/CarData.js
+++ src/api/CarData.js
@@ -6,22 +6,24 @@ function criaLinha(cars) {
 	cylinders = document.createElement("td");
 	year = document.createElement("td");
 	transmission = document.createElement("td");
+	drive=document.createElement("td");
 	fuel_type=document.createElement("td");
-
+	
 	make.innerHTML = cars.make;
 	model.innerHTML = cars.model;
 	cylinders.innerHTML = cars.cylinders;
 	year.innerHTML = cars.year;
 	transmission.innerHTML = cars.transmission;
-	transmission.innerHTML = cars.transmission;
-
+	drive.innerHTML = cars.drive;
+	fuel_type.innerHTML = cars.fuel_type;
+		
 	linha.appendChild(make);
 	linha.appendChild(model);
 	linha.appendChild(cylinders);
 	linha.appendChild(year);
 	linha.appendChild(transmission);
-	linha.appendChild(transmission);
-
+	linha.appendChild(drive);
+	linha.appendChild(fuel_type);
 	return linha;
 }
 
@@ -51,4 +53,25 @@ function ApiCardData(modelo) {
 
 
 }
+
+const options = {
+	method: 'GET',
+	url: 'https://api.api-ninjas.com/v1/cars?limit=30&model=',
+	headers: {
+		'X-Api-Key': 'QC4+leFexAHBjfM5Q+fpog==FgGtErKDP9ALE2VC'
+	},
+};
+
+axios.request(options).then(function (response) {
+	let cars = response.data;
+	let tabela = document.getElementById("tabela")
+	cars.forEach(element => {
+		
+		let linha = criaLinha(element);
+		tabela.appendChild(linha);
+	});
+	
+}).catch(function (error) {
+	console.error(error);
+});
 
